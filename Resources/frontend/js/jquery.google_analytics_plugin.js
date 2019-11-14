a66a7d50c65c59e89f4ff0198c2bf0f5c1edee9b
Dennis Garding <di@shopware.com>
2019-11-14 15:45:53 +0100

    PT-11004 - Fix js error in FireFox

--- Resources/frontend/js/jquery.google_analytics_plugin.js
+++ Resources/frontend/js/jquery.google_analytics_plugin.js
@@ -64,8 +64,8 @@
         },
 
         checkDoNotTrack: function() {
-            if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || 'msTrackingProtectionEnabled' in window.external) {
-                if (window.doNotTrack == "1" || navigator.doNotTrack === "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1" || window.external.msTrackingProtectionEnabled()) {
+            if (window.doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack) {
+                if (window.doNotTrack == "1" || navigator.doNotTrack === "yes" || navigator.doNotTrack == "1" || navigator.msDoNotTrack == "1") {
                     return true;
                 }
             }
